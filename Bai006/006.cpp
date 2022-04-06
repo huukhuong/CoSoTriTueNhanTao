f4ed53396f6067558b5c91e0069362ee59aa8ac8
gameooff111111 <phimtonghop7879@gmail.com>
2022-04-06 07:37:22 +0700

    clean code bai 006

--- Bai006/006.cpp
+++ Bai006/006.cpp
@@ -1,29 +1,39 @@
 #include <iostream>
-#include <fstream>
 #include <cstdio>
 #include <cstdlib>
 #include <algorithm>
 #include <cmath>
 #include <sstream>
 #include <string>
+#include <stdlib.h>
+#include <fstream>
 
 using namespace std;
 
 int n, x[100], dd[100];
-bool yes[100][100];
+bool Prime[100][100];
 int res[100] = { 0, 0, 2, 2, 4, 96, 1024, 2880, 81024, 770144 }; // mang so cach tim duoc vong so nguyen to
-int dem = 0;
-string s = "";
+string s = ""; // ket qua se luu vao bien s va ghi bien s ra file
+
+string numberToString(unsigned int n)
+{
+    stringstream ss;
+    ss << n;
+    return ss.str();
+}
+
+int stringToNumber(string s)
+{
+    return atoi(s.c_str());
+}
 
 int readFile()
 {
     fstream f;
-    f.open("CIRCLE.INP", ios::in); // doc du lieu tu file input.txt
+    f.open("CIRCLE.INP", ios::in); // doc du lieu tu file CIRCLE.INP
     string data;
     getline(f, data);
-    stringstream geek(data); // su dung str stream de luu so n
-    n = 0;
-    geek >> n;
+    n = stringToNumber(data); // lay du lieu tu file xong gan vao bien n
     f.close();
     return n;
 }
@@ -36,6 +46,7 @@ void writeFile(string data)
     f << data;
     f.close();
 }
+
 bool isPrime(int n)
 { // ham kiem tra so nguyen to
     if (n <= 1) {
@@ -52,24 +63,21 @@ bool isPrime(int n)
 void dq(int t)
 {
     if (t == n + 1) {
-        if (!yes[x[1]][x[n]]) {
+        if (!Prime[x[1]][x[n]]) {
             return;
         }
-        dem++;
         for (int i = 1; i <= n; i++) {
-            //	cout<<x[i]<<" ";
-            ostringstream str1;
-            str1 << x[i];
-            string geek = str1.str();
-            s.append(geek);
+            cout << x[i] << " ";
+            string geek = numberToString(x[i]); // chuyen so thanh chu roi
+            s.append(geek); // gan vao bien "s" de luu vao file
             s.append(" ");
         }
         s.append("\n");
-        //cout<<endl;
+        cout << endl;
         return;
     }
     for (int i = 1; i <= n; i++)
-        if (dd[i] == 0 && yes[x[t - 1]][i]) {
+        if (dd[i] == 0 && Prime[x[t - 1]][i]) {
             x[t] = i;
             dd[i] = 1;
             dq(t + 1);
@@ -80,25 +88,34 @@ void dq(int t)
 
 void setdata(int n)
 {
-    for (int i = 1; i <= n; i++)
+    for (int i = 1; i <= n; i++) {
         for (int j = i; j <= n; j++) {
-            yes[i][j] = isPrime(i + j);
-            yes[j][i] = yes[i][j];
+            Prime[i][j] = isPrime(i + j);
+            Prime[j][i] = Prime[i][j]; // sàng so nguyen to dua vao mang
         }
+        // cout<<endl;
+    }
     x[1] = 1;
     dd[1] = 1;
-
-    ostringstream str1;
-    str1 << res[n / 2];
-    string geek = str1.str();
+    //	 for (int i = 0; i < n; i++){
+    //	        for (int j = 0; j < n; j++) {
+    //	           	cout<<"yes["<<i<<"]["<<j<<"] = "<<yes[i][j]<<" ";
+    //	        }
+    //	        cout<<endl;
+    //	}
+    string geek = numberToString(res[n / 2]);
     s.append(geek);
-    s.append("\n");
+    s.append("\n"); // luu so cach tim duoc vao chuoi
 }
 int main()
 {
-    n = readFile(); // gan n = du lieu trong file input.txt
+    n = readFile(); // gan n = du lieu trong file CIRCLE.INP
     n *= 2;
+    cout << res[n / 2] << endl;
     setdata(n);
     dq(2);
     writeFile(s);
+    cout << "Da ghi du lieu vao file CIRCLE.OUT" << endl;
+    system("pause");
+    return 0;
 }
