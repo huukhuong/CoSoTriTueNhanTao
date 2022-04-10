d49852e7c9fb315ff54b2bbbe8fdfb3a271a1565
gameooff111111 <phimtonghop7879@gmail.com>
2022-04-11 01:03:15 +0700

    done

--- Bai006/test2.cpp
+++ Bai006/test2.cpp
@@ -1,25 +1,51 @@
 #include <iostream>
-#include <math.h>
-#include <algorithm>3
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <string>
+#include <stdlib.h>
+#include <fstream>
+
 #define MAX 100
 using namespace std;
 
 int A[MAX][MAX];
 int C[MAX], B[MAX];
 int n;
-int d; // số lượng chu trình
-
-
-void print(int a[100][100])
+int d;      // số lượng chu trình
+int sodinh; // số đỉnh = 2n;
+string s;
+string numberToString(unsigned int n)
 {
-    for (int i = 1; i <= 2 * n; i++)
-    {
-        for (int j = 1; j <= 2 * n; j++)
-        {
-            cout << a[i][j] << " ";
-        }
-        cout << endl;
-    }
+    stringstream ss;
+    ss << n;
+    return ss.str();
+}
+
+int stringToNumber(string s)
+{
+    return atoi(s.c_str());
+}
+
+void readFile() // hàm đọc file
+{
+    fstream f;
+    f.open("CIRCLE.INP", ios::in); // doc du lieu tu file CIRCLE.INP
+    string data;
+    getline(f, data);
+    n = stringToNumber(data); // lay du lieu tu file xong gan vao bien n
+    f.close();
+}
+
+void writeFile(string data)
+{
+
+    fstream f;
+    f.open("CIRCLE.OUT", ios::out); // ghi du lieu ra file CIRCLE.OUT
+    f << data;
+    f.close();
 }
 
 void init(int n) // khởi tạo giá trị mặc định của chu trình
@@ -53,32 +79,37 @@ void matran(int n) // khởi tạo ma trận kề
             int sum = i + j;
             int diff = abs(i - j);
             if (isPrime(sum))
-            {  
+            {
                 A[i][j] = 1;
             }
         }
     }
 }
 
-void Result(void) // in chu trình 
+void Result(void) // in chu trình
 {
 
-    for (int i = n; i > 0; i--)
+    for (int i = sodinh; i > 0; i--)
+    {
         cout << B[i] << " ";
+        s.append(numberToString(B[i]));
+        s.append(" ");
+    }
     d++; // tăng biến đếm các chu trình có thể tìm được
     cout << endl;
+    s.append("\n");
 }
 
 void Hamilton(int *B, int *C, int i) // duyệt chu trình Hamilton với tham số đầu vào là Ma trận kề A
 {
     int j, k;
-    for (j = 1; j <= n; j++)
+    for (j = 1; j <= sodinh; j++)
     {
         if (A[B[i - 1]][j] == 1 && C[j] == 0)
         {
             B[i] = j;
             C[j] = 1;
-            if (i < n)
+            if (i < sodinh)
                 Hamilton(B, C, i + 1);
             else if (B[i] == B[0])
                 Result();
@@ -89,13 +120,15 @@ void Hamilton(int *B, int *C, int i) // duyệt chu trình Hamilton với tham s
 
 int main()
 {
-    B[0] = 1; 
-    cin >> n;
-    int i = 1;
-    d = 0;
-    init(n);
-    matran(n);
-    n = 2 * n;
-    Hamilton(B, C, i);
-    cout << d;
+    B[0] = 1;
+    readFile();
+    int i = 1; // xuất phát từ đỉnh 1
+    d = 0;     // gán biến đếm số lượng chu trình = 0 ;
+    init(n);   // khởi tạo ma trận
+    matran(n); // tạo ma trận kề
+    sodinh = 2 * n;
+    Hamilton(B, C, i); // chuyển ma trận kề thành chu trình
+    cout<<d;
+    
+   
 }
